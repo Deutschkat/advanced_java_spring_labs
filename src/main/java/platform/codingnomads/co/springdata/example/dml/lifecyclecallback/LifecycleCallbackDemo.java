ab19836189ac108abb60b704594b40e25d9eabf4
Kat Nix <deutschkat29@gmail.com>
2023-05-08 13:58:54 -0500

    Spring data stuff

--- src/main/java/platform/codingnomads/co/springdata/example/dml/lifecyclecallback/LifecycleCallbackDemo.java
+++ src/main/java/platform/codingnomads/co/springdata/example/dml/lifecyclecallback/LifecycleCallbackDemo.java
@@ -15,8 +15,13 @@ public class LifecycleCallbackDemo {
     @Bean
     public CommandLineRunner runStuff(PrintEntityRepository printEntityRepository) {
         return (args) -> {
-            // put your logic here
+            PrintEntity entity1 = new PrintEntity();
+            entity1.setName("Entity 1");
+            printEntityRepository.save(entity1);
 
+            PrintEntity entity2 = new PrintEntity();
+            entity2.setName("Entity 2");
+            printEntityRepository.save(entity2);
         };
     }
 }
--- src/main/java/platform/codingnomads/co/springdata/example/dml/lifecyclecallback/PrintEntity.java
+++ src/main/java/platform/codingnomads/co/springdata/example/dml/lifecyclecallback/PrintEntity.java
@@ -4,10 +4,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.PrePersist;
+import javax.persistence.*;
 
 @Entity
 @Getter
@@ -19,6 +16,37 @@ public class PrintEntity {
     @GeneratedValue
     private Long id;
 
-    // write your methods here
+    private String name;
+
+
+    @PrePersist
+    public void beforePersist() {
+        System.out.println("Before persisting: " + name);
+    }
+
+    @PostPersist
+    public void onPersist() {
+        System.out.println("Persisted: " + name);
+    }
+
+    @PreUpdate
+    public void beforeUpdate() {
+        System.out.println("Before updating: " + name);
+    }
+
+    @PostUpdate
+    public void onUpdate() {
+        System.out.println("Updated: " + name);
+    }
+
+    @PreRemove
+    public void beforeRemove() {
+        System.out.println("Before removing: " + name);
+    }
+
+    @PostRemove
+    public void onRemove() {
+        System.out.println("Removed: " + name);
+    }
 
 }
