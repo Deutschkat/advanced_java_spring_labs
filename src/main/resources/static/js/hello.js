c75258d23448d8c36e5f2e4d24fd11ac6c22c187
Kat Nix <deutschkat29@gmail.com>
2023-05-28 16:23:11 -0500

    MVC

--- src/main/resources/static/js/hello.js
+++ src/main/resources/static/js/hello.js
@@ -2,3 +2,13 @@ function greeting() {
     alert("Hello Spring Developer!!");
 }
 
+function meow() {
+    alert("License and registration, right meow.");
+}
+
+function high5() {
+    alert("Yeah!! Thanks for the virtual high 5!!");
+}
+
+
+
