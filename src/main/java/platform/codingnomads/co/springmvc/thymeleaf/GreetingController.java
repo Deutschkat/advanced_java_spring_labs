e9985bdec155d9e02afbc63d72a430360d483894
Kat Nix <deutschkat29@gmail.com>
2023-05-28 15:53:37 -0500

    MVC

--- src/main/java/platform/codingnomads/co/springmvc/thymeleaf/GreetingController.java
+++ src/main/java/platform/codingnomads/co/springmvc/thymeleaf/GreetingController.java
@@ -15,6 +15,19 @@ public class GreetingController {
         return "greeting"; 
     }
 
+    @GetMapping("/practice")
+    public String indexPractice(Model model){
+        model.addAttribute("name", "Kat");
+        model.addAttribute("age", 30);
+        model.addAttribute("favorite_food", "seafood");
+        model.addAttribute("languages", "German, Italian, English");
+        model.addAttribute("pets", "2 dogs");
+
+
+        return "practice";
+    }
+
+
     @GetMapping("/subjects")
     public String subjects(Model model) {
         Subject s1 = new Subject("Java", "Programming");
